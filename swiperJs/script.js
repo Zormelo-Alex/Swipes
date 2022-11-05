814fc71c39a9b0c7ccb8c636dc2a290f768070c3
Zormelo_Alex <alexzormelo9@gmail.com>
2022-11-04 17:18:27 -0700

    first commit

---
+++ swiperJs/script.js
@@ -0,0 +1,26 @@
+console.log("working!!!");
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    autoplay:{
+        delay: 2000,
+    },
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: "true",
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
